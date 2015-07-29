ec4116742f53c3fe790c5f6162334589027fbae2
Bill Currie <bill@taniwha.org>
2015-07-29 20:22:46 +0900

    Get basic zip creation working.
    Using git generated version info in the file name :)

--- tools/git-version.sh
+++ tools/git-version.sh
@@ -2,9 +2,3 @@
 
 full_version=`./tools/git-version-gen --prefix v .tarball-version`
 echo $full_version
-version=`echo $full_version | sed -e 's/-/\t/' | cut -f 1`
-
-MAJOR=`echo $full_version | cut -f 1 -d .`
-MINOR=`echo $full_version | cut -f 2 -d .`
-PATCH=`echo $full_version | cut -f 3 -d .`
-BUILD=`echo $full_version | cut -f 4 -d . | cut -f 1 -d '-'`
